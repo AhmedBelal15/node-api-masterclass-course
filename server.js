d93c73d9d8dc31b21b496e56151e6577f7eea67c
ahmedbelal15 <ahmbelal95@gmail.com>
2021-07-18 19:48:17 +0200

    database connection + Bootcamp model

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const morgan = require('morgan');
+const colors = require('colors')
+const connectDB = require('./config/db')
 // Load ENV Vars
 dotenv.config({ path: "./config/config.env" });
 
+// Connect to DB
+connectDB()
 const app = express();
+
+//Dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+app.get('/api/v1/bootcamps', (req,res)=>{res.json('saasdasdasd')})
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold);
 });
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise)=>{
+  console.log(`Error: ${err.message}`.red.bold);
+  //Close Server
+  server.close(()=>{
+    process.exit(1)
+  })
+})
