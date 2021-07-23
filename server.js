b03c08ce4ae381eda887a201aaf0831b74ab939f
ahmedbelal15 <ahmbelal95@gmail.com>
2021-07-23 15:01:02 +0200

    bootcamps controllers and routes

--- server.js
+++ server.js
@@ -8,20 +8,32 @@ dotenv.config({ path: "./config/config.env" });
 
 // Connect to DB
 connectDB()
+
+// Route files
+const bootcamps = require('./routes/bootcamps')
+
 const app = express();
+// Body Parser
+app.use(express.json())
 
 //Dev logging middleware
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 
-app.get('/api/v1/bootcamps', (req,res)=>{res.json('saasdasdasd')})
+
+
+// Mount routes
+app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold);
 });
+
+
+
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise)=>{
