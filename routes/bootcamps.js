d8b752ded7b8e3593008d15f28426a29d4e07b31
ahmedbelal15 <ahmbelal95@gmail.com>
2021-07-31 21:47:32 +0200

    get bootcamps in radius

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { createBootcamp, getBootcamps, getBootcamp, updateBootcamp, deleteBootcamp } = require("../controllers/bootcamps");
+const { createBootcamp, getBootcamps, getBootcamp, updateBootcamp, deleteBootcamp, getBoocampsInRadius } = require("../controllers/bootcamps");
 
+router.route('/radius/:zipcode/:distance').get(getBoocampsInRadius)
 router.route('/').get(getBootcamps).post(createBootcamp)
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp)
 module.exports = router;
