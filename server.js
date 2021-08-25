31c6caa15ea4a0fa067562036d27b363980bfaab
ahmedbelal15 <ahmbelal95@gmail.com>
2021-08-25 02:59:03 +0200

    rate limiting & prevent http param pollution

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const cookieParser = require('cookie-parser')
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet')
 const xss = require('xss-clean')
+const expressRateLimit = require('express-rate-limit')
+const hpp = require('hpp')
 const errorHandler = require("./middleware/errorHandler");
 const connectDB = require("./config/db");
 // Load ENV Vars
@@ -46,6 +48,17 @@ app.use(helmet())
 
 // Prevent XSS Attacks
 app.use(xss())
+
+// Rate limiting
+const limiter = expressRateLimit({
+  windowMs: 10*60*1000, // 10 minutes
+  max: 100
+})
+
+app.use(limiter)
+
+// Prevent http param pollution
+app.use(hpp())
 
 //Set static folder
 app.use(express.static(path.join(__dirname, "public")));
